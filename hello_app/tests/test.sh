27cc7467950c7021c64cecc2678bf47f6549a505
dontworrygh <128793290+dontworrygh@users.noreply.github.com>
2023-07-05 12:27:50 +0300

    Update test.sh

--- hello_app/tests/test.sh
+++ hello_app/tests/test.sh
@@ -5,8 +5,8 @@ output=$(curl http://127.0.0.1:5000)
 expected_output="05.07.2023"
 
 if [ "$output" == "$expected_output" ]; then
-echo "Вывод программы совпадает с ожидаемым: $output"
+  echo "Вывод программы совпадает с ожидаемым: $output"
 else
-echo "Вывод программы не совпадает с ожидаемым.
-exit 1
+  echo "Вывод программы не совпадает с ожидаемым.
+  exit 1
 fi
