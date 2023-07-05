d8c17532c600523ac40a3a86b7f805a3874b1c6b
dontworrygh <128793290+dontworrygh@users.noreply.github.com>
2023-07-05 16:13:22 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,11 +1,5 @@
-import os
-from flask import Flask, request
+from datetime import date
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return f'05.07.2023'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+today = date.today()
+formatted_date = today.strftime('%Y-%m-%d')
+print(formatted_date)
