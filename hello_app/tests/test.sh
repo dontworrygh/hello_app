8a94749e44eadab1c912f9bd677dffb064a1c604
dontworrygh <128793290+dontworrygh@users.noreply.github.com>
2023-07-05 12:25:33 +0300

    Update test.sh

--- hello_app/tests/test.sh
+++ hello_app/tests/test.sh
@@ -7,6 +7,6 @@ expected_output="05.07.2023"
 if [ "$output" == "$expected_output" ]; then
   echo "Вывод программы совпадает с ожидаемым: $output"
 else
-  echo "Вывод программы не совпадает с ожидаемым.
+  echo "Вывод программы не совпадает с ожидаемым."
   exit 1
 fi
