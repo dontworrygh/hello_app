77ed476371a5f49973825cd04b41f20cfc827b74
dontworrygh <128793290+dontworrygh@users.noreply.github.com>
2023-07-05 16:12:35 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -1,11 +1,17 @@
 import os
+from datetime import date
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'05.07.2023'
+    return f'My server IP address is {os.environ["HOST_IP"]}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
+today = date.today()
+formatted_date = today.strftime('%Y-%m-%d')
+print(formatted_date)
